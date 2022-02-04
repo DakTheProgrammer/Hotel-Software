3e85c402a0cefe933f96836cd2df318b889e3ca4
Dakota Wilson <dtw3200@live.com>
2022-02-04 15:03:54 -0600

    Database updates

--- Classes/EasySQL.py
+++ Classes/EasySQL.py
@@ -11,7 +11,8 @@ class SQL:
             self.con.commit()
             return self.cur.fetchall()
         except:
-            return 'Bad query'
+            print('Bad query')
+            raise
 
     def printQuery(self, query):
         try:
@@ -19,14 +20,41 @@ class SQL:
                 print(row)
         except:
             print('Bad query')
+            raise
 
     def insert(self, table, values):
-        ...
+        try:
+            col = self.getColumns(table)
+            query = 'INSERT INTO %s (%s) VALUES(%s)' % (table, ', '.join(col), '?,' * (len(values) - 1) + '?')
+            self.cur.execute(query, values)
+            self.con.commit()
+        except:
+            raise
 
-    def update(self, table, key, values):
-        ...
-    
-    def delete(self, table, key):
-        ...
+    def getTables(self):
+        try:
+            self.cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+            return self.cur.fetchall()
+        except:
+            print('Invalid table')
+            raise
+
+    def getColumns(self, table):
+        try:
+            ret = []
+            for col in self.cur.execute("SELECT * FROM %s" % table).description:
+                ret.append(col[0])
+            return(ret)
+        except:
+            print('Invalid table')
+            raise
+
+    def getAllFromTable(self, table):
+        try:
+            self.cur.execute("SELECT * FROM %s" % table)
+            return self.cur.fetchall()
+        except:
+            print('Invalid table')
+            raise
 
 DB = SQL()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from GUIs.Login.Login import LoginApp
+from Classes.EasySQL import DB
 
 if __name__ == '__main__':
-    LoginApp().run()
-    
+    #LoginApp().run()
+    ...
