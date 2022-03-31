45b4057b1599d26197ffcdd482cdbd7f5495554a
Dakota Wilson <dtw3200@live.com>
2022-03-31 17:26:24 -0500

    Bug fixes on interface

--- GUIs/Python/ManagerInterface.py
+++ GUIs/Python/ManagerInterface.py
@@ -5,19 +5,24 @@ from kivy.metrics import dp
 from Classes.EasySQL import DB
 
 class ManagerInterfacePage(Screen):
-    def on_pre_enter(self):
-        table = MDDataTable(
-            pos_hint = {'center_x': 0.5, 'center_y': 0.5},
-			size_hint =(0.9, 0.6),
-            check = True,
-            column_data = [
-                ("First Name", dp(30)),
-                ("Last Name", dp(30))
-            ],
-            row_data = [
-                ('Dakota', 'Wilson'),
-                ('John', 'Doe')
-            ]
-        )
+    table = None 
 
-        self.add_widget(table)
+    def on_pre_enter(self):
+        #needed for a bug fix with kivy
+        if self.table == None:
+            self.table = MDDataTable(
+                pos_hint = {'center_x': 0.5, 'center_y': 0.5},
+                size_hint =(0.9, 0.6),
+                check = True,
+                column_data = [
+                    ("First Name", dp(30)),
+                    ("Last Name", dp(30))
+                ],
+                row_data = [
+                    ('Dakota', 'Wilson'),
+                    ('John', 'Doe')
+                ]
+            )
+
+            self.add_widget(self.table)
+        
