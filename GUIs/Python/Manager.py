17cbd8acdd16b09424515046d98a5ec53f240c1d
Dakota Wilson <dtw3200@live.com>
2022-03-31 17:09:38 -0500

    Genreal layout for manager

--- GUIs/Python/Manager.py
+++ GUIs/Python/Manager.py
@@ -1,6 +1,17 @@
+from turtle import update
 from kivy.uix.screenmanager import Screen
+from kivy.clock import Clock
+from datetime import date, datetime
 
 from Classes.EasySQL import DB
 
 class ManagerPage(Screen):
-    pass
+    def on_pre_enter(self):
+        self.ids.clock.text = datetime.today().strftime("%I:%M %p")
+        self.ids.date.text = date.today().strftime("%A, %B %d %Y")
+        Clock.schedule_interval(self.updates, 0.5)
+
+    def updates(self, args):
+        self.ids.clock.text = datetime.today().strftime("%I:%M %p")
+        self.ids.date.text = date.today().strftime("%A, %B %d %Y")
+        
