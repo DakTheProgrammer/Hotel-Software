2a9b45b3fea4de3c418be705343a0783ddc8197c
Dakota Wilson <dtw3200@live.com>
2022-04-04 14:50:10 -0500

    Initial interface still needs functionality

--- GUIs/Python/EditServices.py
+++ GUIs/Python/EditServices.py
@@ -1,6 +1,27 @@
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
 
 from Classes.EasySQL import DB
 
 class EditServicesPage(Screen):
-    pass
+    table = None 
+
+    def on_pre_enter(self):
+        #needed for a bug fix with kivy
+        if self.table == None:
+            self.table = MDDataTable(
+                pos_hint = {'center_x': 0.5, 'center_y': 0.575},
+                size_hint =(0.9, 0.75),
+                check = True,
+                column_data = [
+                    ("Item", dp(45)),
+                    ("Amount", dp(45)),
+                    ('Type', dp(45)),
+                ],
+                row_data = [
+                    ('12oz ribeye', '15', 'Food')
+                ]
+            )
+
+            self.add_widget(self.table)
--- GUIs/Python/Orders.py
+++ GUIs/Python/Orders.py
@@ -1,6 +1,27 @@
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
 
 from Classes.EasySQL import DB
 
 class OrdersPage(Screen):
-    pass
+    table = None 
+
+    def on_pre_enter(self):
+        #needed for a bug fix with kivy
+        if self.table == None:
+            self.table = MDDataTable(
+                pos_hint = {'center_x': 0.5, 'center_y': 0.575},
+                size_hint =(0.9, 0.75),
+                check = True,
+                column_data = [
+                    ("Guest", dp(45)),
+                    ("Room", dp(45)),
+                    ('Type', dp(45)),
+                ],
+                row_data = [
+                    ('Dakota Wilson', '420', 'Food')
+                ]
+            )
+
+            self.add_widget(self.table)
