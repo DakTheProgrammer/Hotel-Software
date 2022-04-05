8b2819d1f8585c1970b8a90a52552b50ac7a5dd3
Dakota Wilson <dtw3200@live.com>
2022-04-05 18:38:29 -0500

    Front end for bellhop good

--- GUIs/Python/BellGuest.py
+++ GUIs/Python/BellGuest.py
@@ -1,6 +1,29 @@
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
 
 from Classes.EasySQL import DB
 
 class BellGuestPage(Screen):
-    pass
+    table = None 
+
+    def on_pre_enter(self):
+        #needed for a bug fix with kivy
+        if self.table == None:
+            self.table = MDDataTable(
+                pos_hint = {'center_x': 0.5, 'center_y': 0.575},
+                size_hint =(0.9, 0.75),
+                check = False,
+                column_data = [
+                    ("First Name", dp(30)),
+                    ("Last Name", dp(30)),
+                    ('Room', dp(20)),
+                    ('Type', dp(30)),
+                    ('Lugage', dp(25))
+                ],
+                row_data = [
+                    ('Dakota', 'Wilson', '420', 'Gold Member', 'Not Taken')
+                ]
+            )
+
+            self.add_widget(self.table)
--- GUIs/Python/BellImportant.py
+++ GUIs/Python/BellImportant.py
@@ -1,6 +1,27 @@
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
 
 from Classes.EasySQL import DB
 
 class BellImportantPage(Screen):
-    pass
+    table = None 
+
+    def on_pre_enter(self):
+        #needed for a bug fix with kivy
+        if self.table == None:
+            self.table = MDDataTable(
+                pos_hint = {'center_x': 0.5, 'center_y': 0.575},
+                size_hint =(0.9, 0.75),
+                check = False,
+                column_data = [
+                    ("First Name", dp(45)),
+                    ("Last Name", dp(45)),
+                    ('Role', dp(45))
+                ],
+                row_data = [
+                    ('Dakota', 'Wilson', 'CEO')
+                ]
+            )
+
+            self.add_widget(self.table)
