e05645a9365d73f78f72965016cdb39a6885e26f
apwarren <allywarren7@gmail.com>
2022-04-05 20:35:07 -0500

    Added checking in and confirmation for it

--- GUIs/Python/Guest.py
+++ GUIs/Python/Guest.py
@@ -2,17 +2,19 @@ from kivy.uix.screenmanager import Screen
 from kivy.clock import Clock
 from datetime import date, datetime
 
-
 from Classes.EasySQL import DB
 
 class GuestPage(Screen):
+
     def on_pre_enter(self):
         self.ids.clock.text = datetime.today().strftime("%I:%M %p")
         self.ids.date.text = date.today().strftime("%a., %b %d %Y")
-        self.ids.room.text = ' Room: Not Checked In'  #Temporary replace with room given to guest
         Clock.schedule_interval(self.updates, 0.5)
 
     def updates(self, args):
         self.ids.clock.text = datetime.today().strftime("%I:%M %p")
         self.ids.date.text = date.today().strftime("%a., %b %d %Y")
-        
+
+    def checkin(self):
+        #Temporary until able to see which rooms are available
+        self.ids.room.text = ' Room: 118'
