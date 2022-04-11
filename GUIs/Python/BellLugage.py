8648447962e222e57080b1260e4385ca669273b1
Dakota Wilson <dtw3200@live.com>
2022-04-11 13:39:02 -0500

    Bellhop functionallity finished

--- GUIs/Python/BellLugage.py
+++ GUIs/Python/BellLugage.py
@@ -1,6 +1,7 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.datatables import MDDataTable
 from kivy.metrics import dp
+from kivy.uix.image import Image
 
 from Classes.EasySQL import DB
 
@@ -8,8 +9,21 @@ class BellLugagePage(Screen):
     table = None 
 
     def on_pre_enter(self):
+        self.loading = Image(
+            source = 'images/Loading.jpg',
+            size_hint = (None, None),
+            size = (400, 400),
+            pos_hint = {'center_x': 0.5, 'center_y': 0.575},
+            )
+
+        self.add_widget(self.loading)
+
+    def on_enter(self):
         #needed for a bug fix with kivy
         if self.table == None:
+            query = 'SELECT x.First, x.Last, y.Room, y.Status, y.Location FROM Guest AS x JOIN Bags AS y ON (x.Username = y.Username) WHERE y.Location != "None"'
+            info = DB.run(query)
+
             self.table = MDDataTable(
                 pos_hint = {'center_x': 0.5, 'center_y': 0.575},
                 size_hint =(0.9, 0.75),
@@ -22,9 +36,41 @@ class BellLugagePage(Screen):
                     ('Bag Location', dp(30)),
                 ],
                 row_data = [
-                    ('Dakota', 'Wilson', '420', 'Available', 'Front Door'),
-                    ('Eren', 'Yeager', '690', 'In Transit', 'In Transit')
-                ]
+                    
+                ],
             )
 
+            for items in info:
+                self.table.row_data.append(items)
+
             self.add_widget(self.table)
+
+        self.remove_widget(self.loading)
+
+    def up(self):
+        self.remove_widget(self.table)
+        self.table = None
+        self.parent.current = 'BellhopPage' #this line right here says what is wrong with kivy as a GUI library
+        self.parent.current = 'BellLugagePage'
+
+    def take(self):
+        rows = self.table.get_row_checks()
+
+        if rows != []:
+            
+            for row in rows:
+                query = f'UPDATE Bags SET status = "Taken", Location = "Taken" WHERE Room = "{row[2]}"'
+                DB.run(query)
+      
+            self.up()
+
+    def complete(self):
+        rows = self.table.get_row_checks()
+
+        if rows != []:
+            
+            for row in rows:
+                query = f'UPDATE Bags SET status = "Taken", Location = "None" WHERE Room = "{row[2]}"'
+                DB.run(query)
+      
+        self.up()
