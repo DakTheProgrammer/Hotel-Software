795e10ec221ade357f33f30096100da504f553c4
Dakota Wilson <dtw3200@live.com>
2022-04-12 19:38:29 -0500

    DB changes

---
+++ SQLite/SQL Files/Nuke.sql
@@ -0,0 +1,5 @@
+DELETE FROM Users WHERE (Username != 'dtw' AND Username != 'awrn' AND Username != 'chadc' AND Username != 'dtwman' AND Username != 'dtwatt' AND Username != 'dtwbell' AND Username != 'dtwroom');
+DELETE FROM Employee;
+DELETE FROM Guest;
+DELETE FROM Room;
+DELETE FROM Bags;
